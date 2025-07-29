84d363f2216ca76fdfa98161a99f50c48c38e975
Faramarz <faramarz@scal.io>
2025-07-30 00:28:50 +0330

    fix(jobs): add external job item duplicate removal

--- src/jobs/services/jobs.service.ts
+++ src/jobs/services/jobs.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { JobsExternalApiService } from './jobs-external-api.service';
 import { JobsRepository } from '../repositories/jobs.repositories';
 import { JobExternalSource1DtoToEntityMapper, JobExternalSource2DtoToEntityMapper } from '../mappers';
-import { JobExternalSource1ResponseDto, JobExternalSource2ResponseDto, JobsListRequest, JobsListResponseDto } from '../dto';
+import { JobExternalSource1ApiResponseDto, JobExternalSource2ApiResponseDto, JobsListRequest, JobsListResponseDto } from '../dto';
 import { JobEntity } from '../entities/job.entity';
 import { JobsListQueryBuilder } from '../utils/list-query-builder';
 import { PinoLogger } from 'nestjs-pino';
@@ -17,6 +17,11 @@ export class JobsService {
         private readonly logger: PinoLogger,
     ) { }
 
+    /**
+     * 
+     * @param request 
+     * @returns 
+     */
     async list(request: JobsListRequest): Promise<JobsListResponseDto> {
         const page = request.page || 1;
         const pageSize = request.pageSize || 10;
@@ -39,14 +44,36 @@ export class JobsService {
         };
     }
 
+    /**
+     * Description: fetch external job endpoints and update the database with the new job items. there will be no duplicates
+     */
     async fetchExternalJobSources() {
-        const jobSource1: JobExternalSource1ResponseDto = await this.externalJobsApi.fetchSource1();
-        const jobSource2: JobExternalSource2ResponseDto = await this.externalJobsApi.fetchSource2();
+        const jobSource1: JobExternalSource1ApiResponseDto = await this.externalJobsApi.fetchSource1();
+        const jobSource2: JobExternalSource2ApiResponseDto = await this.externalJobsApi.fetchSource2();
         const entities: JobEntity[] = [
             ...this.externalDtoSource1ToEntityMapper.mapDtoToEntities(jobSource1.jobs),
             ...this.externalDtoSource2ToEntityMapper.mapDtoToEntities(jobSource2.data.jobsList)
         ];
 
-        await this.repository.upsert(entities);
+        await this.repository.upsert(this.filterDuplicateJobs(entities));
+    }
+
+    /**
+     * Filter jobs by JobId to prevent duplicates
+     * @param jobs 
+     * @returns 
+     */
+    private filterDuplicateJobs(jobs: JobEntity[]): JobEntity[] {
+        const jobIdSet: Set<string> = new Set<string>();
+        const uniqueJobs: JobEntity[] = [];
+
+        for (const job of jobs) {
+            if (!jobIdSet.has(job.jobId)) {
+                jobIdSet.add(job.jobId);
+                uniqueJobs.push(job);
+            }
+        }
+
+        return uniqueJobs;
     }
 }
