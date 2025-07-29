48e13d1ba46efd028fed9f640b5b18c1c5dbae36
Faramarz <faramarz@scal.io>
2025-07-29 10:02:32 +0330

    feat(core): config and typeOrm modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import * as path from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: path.join(process.cwd(), '/envs', `${process.env.NODE_ENV??''}.env`)
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
